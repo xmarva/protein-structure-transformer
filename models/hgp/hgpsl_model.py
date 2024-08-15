4b677b807cf5a24f367defee5a0bb59418b50e32
Elizabeth <arcsinx@192.168.1.5>
2024-08-15 18:07:06 -0300

    typo fix

--- models/hgp/hgpsl_model.py
+++ models/hgp/hgpsl_model.py
@@ -3,7 +3,7 @@ import torch.nn.functional as F
 from torch_geometric.nn import global_mean_pool as gap, global_max_pool as gmp
 from torch_geometric.nn import GCNConv
 
-from models.hgnn_model.layers import GCN, HGPSLPool
+from models.hgp.layers import GCN, HGPSLPool
 
 
 class Model(torch.nn.Module):
