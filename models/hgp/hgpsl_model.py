814d3948295a9cdc27118582664356417b756e0e
Elizabeth <arcsinx@192.168.1.5>
2024-08-15 20:14:06 -0300

    graphs

--- models/hgp/hgpsl_model.py
+++ models/hgp/hgpsl_model.py
@@ -35,6 +35,9 @@ class Model(pl.LightningModule):
         x, edge_index, batch = data.x, data.edge_index, data.batch
         edge_attr = None
 
+        # Apply global pooling
+        x = torch.cat([gmp(x, batch), gap(x, batch)], dim=1)
+
         x = F.relu(self.conv1(x, edge_index, edge_attr))
         x, edge_index, edge_attr, batch = self.pool1(x, edge_index, edge_attr, batch)
         x1 = torch.cat([gmp(x, batch), gap(x, batch)], dim=1)
